9b17645ae08c3a4f70b032976a7506b0c27d30a0
wangruiyao <wangry@asiainfo.com>
2019-04-16 12:06:41 +0800

    citypicker

--- examples/components/tkCityPicker/TkCityPicker.vue
+++ examples/components/tkCityPicker/TkCityPicker.vue
@@ -3,16 +3,24 @@
     <tk-header>
       <tk-search place-holder="城市中文名或拼音"></tk-search>
     </tk-header>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
   import cityList from 'core/city.js'
+  console.log(cityList)
   import TkHeader from "../tkHeader/TkHeader";
   import TkSearch from '../common/TkSearch'
+  import CityList from "./components/CityList";
   export default {
     name: "TkCityPicker",
-    components: {TkHeader, TkSearch}
+    components: {CityList, TkHeader, TkSearch},
+    data() {
+      return {
+        cityList
+      }
+    }
   }
 </script>
 
---
+++ examples/components/tkCityPicker/components/CityList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="tk-city-picker-list">
+    <tk-scroll ref="wrapper"
+               :listenScroll="true"
+               :pullup="true"
+               :data="cityList"
+               >
+      <div v-for="item in cityList">{{item.city}}</div>
+    </tk-scroll>
+  </div>
+</template>
+
+<script>
+  import cityList from 'core/city.js'
+  import TkScroll from "../../tkScroll/TkScroll";
+  export default {
+    name: "CityList",
+    components: {TkScroll},
+    data() {
+      return {
+        cityList
+      }
+    },
+    mounted() {
+      console.log(this.cityList)
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
